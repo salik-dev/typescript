5ccc5d864aadda195b3b8e17f1efe2f6c18300e9
sa-lik <balisaalik@gmail.com>
2024-02-11 09:25:57 +0500

    'salik-commit'

---
+++ sandbox.js
@@ -0,0 +1,9 @@
+// basic types (01)
+// const username = "bahadur ali";
+// const isLoggedIn = false;
+// isLoggedIn = ""
+// username = 20
+// console.log(username)
+// ary & obj types
+var elements = [10, 20, 30];
+console.log(elements);
---
+++ sandbox.ts
@@ -0,0 +1,14 @@
+// basic types (01)
+// const username = "bahadur ali";
+// const isLoggedIn = false;
+
+// isLoggedIn = ""
+// username = 20
+
+// console.log(username)
+
+// ary & obj types
+
+const elements = [10, 20, 30]
+
+console.log(elements)
