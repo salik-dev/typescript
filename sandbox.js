b483b3bd86ef49242754a31d2d9b2daaf19639c8
sa-lik <balisaalik@gmail.com>
2024-02-11 09:38:30 +0500

    'salik-commit'

--- sandbox.js
+++ sandbox.js
@@ -1,9 +1,21 @@
 // basic types (01)
-// const username = "bahadur ali";
-// const isLoggedIn = false;
-// isLoggedIn = ""
-// username = 20
-// console.log(username)
-// ary & obj types
+var username = "bahadur ali";
+var isLoggedIn = false;
+isLoggedIn = ""; // ts not permitted
+username = 20; // ts not permitted
+console.log(username);
+// ary types
 var elements = [10, 20, 30];
-console.log(elements);
+var mixed = [20, false, 'bahadur ali'];
+elements = [20, false, 'bahadur ']; // ts not permitted
+mixed.push(true);
+mixed[3] = 200 / 3939;
+console.log(mixed);
+// objects types
+var userData = {
+    name: "bahadur ali",
+    age: 23,
+    jobStatus: false
+};
+userData.name = "irfan ali";
+console.;
--- sandbox.ts
+++ sandbox.ts
@@ -1,14 +1,31 @@
 // basic types (01)
-// const username = "bahadur ali";
-// const isLoggedIn = false;
+const username = "bahadur ali";
+const isLoggedIn = false;
 
-// isLoggedIn = ""
-// username = 20
+isLoggedIn = "" // ts not permitted
+username = 20 // ts not permitted
 
-// console.log(username)
+console.log(username)
 
-// ary & obj types
+// ary types
 
-const elements = [10, 20, 30]
+const elements = [10, 20, 30];
+const mixed = [20, false, 'bahadur ali']
 
-console.log(elements)
+elements = [20, false, 'bahadur '] // ts not permitted
+mixed.push(true)
+mixed[3] = 200/3939
+
+console.log(mixed)
+
+// objects types
+
+const userData = {
+    name: "bahadur ali",
+    age: 23,
+    jobStatus: false
+}
+
+userData.name = "irfan ali";
+
+console.
